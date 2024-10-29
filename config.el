1c4ac7f23d4ead73133f61cdef86e94096acbbb1
sarathprasath <sarath@kula.ai>
2024-10-29 11:26:36 +0530

    Update config.el

--- config.el
+++ config.el
@@ -37,8 +37,8 @@
   :config
   (setq calendar-latitude 12.9542)   ; Set your latitude
   (setq calendar-longitude 80.2513) ; Set your longitude
-  (setq circadian-themes '((:sunrise . doom-gruvbox)
-                           (:sunset  . doom-solarized-dark)))
+  (setq circadian-themes '((:sunrise . doom-solarized-dark)
+                           (:sunset  . doom-one)))
   (circadian-setup))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
